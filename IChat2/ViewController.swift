fc3c9797857cb3bfdddd40247c090c2c07691309
Muhammad Sattarov <sattarovmuhammad98@gmail.com>
2024-02-20 17:34:04 +0500

    Initial Commit

---
+++ IChat2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IChat2
+//
+//  Created by user on 20/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
